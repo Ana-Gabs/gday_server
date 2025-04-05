d18141e60f98524da7e0408823b6c35447f32247
Ana-Gabs <2022371069@uteq.edu.mx>
2025-04-05 11:14:27 -0600

    "cambios 2"

--- clases/index.js
+++ clases/index.js
@@ -6,14 +6,31 @@ app.use(express.json());
 app.use(cors());
 require('dotenv').config({ path: '../gateway/.env' });
 const { connectToMongo, getDb } = require('./config/db');
-const validarActividad = require('./middlewares/validarActividad');
 
 // Función para validar ObjectId
 function isValidObjectId(id) {
     return ObjectId.isValid(id);
 }
 
-
+// Obtener una actividad por id
+app.get('/clases/:id', async (req, res) => {
+    const { id } = req.params;
+    if (!isValidObjectId(id)) {
+        return res.status(400).json({ message: 'ID de actividad inválido' });
+    }
+    try {
+        const db = getDb();
+        const actividad = await db.collection('actividades').findOne({ _id: new ObjectId(id) });
+        if (actividad) {
+            res.status(200).json(actividad);
+        } else {
+            res.status(404).json({ message: 'Actividad no encontrada' });
+        }
+    } catch (error) {
+        console.error('Error al obtener la actividad:', error);
+        res.status(500).json({ message: 'Error al obtener la actividad' });
+    }
+});
 
 // Iniciar el servidor
 const PORT = process.env.PORT_CLASES || 3007; 
